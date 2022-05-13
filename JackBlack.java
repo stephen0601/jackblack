655a3fb22e80b5a6522adc066573471d78349180
stephen0601 <100398250+stephen0601@users.noreply.github.com>
2022-05-13 19:37:27 -0400

    Update JackBlack.java

--- JackBlack.java
+++ JackBlack.java
@@ -1,214 +1,193 @@
 import java.util.Scanner;
-
-
-
 public class JackBlack {
- //setting the player/dealer wins to 0
+
 	static public int playerCounter = 0 ; 
-
 	static public int dealerCounter = 0 ; 
-
 	static public int numHands = 1 ; 
-    //setting the number of hands to 1
- 
-	
-
+	//sets the round to 1 and player and dealer wins to 0
 	public static void main(String[] args) {
-
-		System.out.println("Let's play some JackBlack!");
-
+		System.out.println("Let's play some jackblack!");
+		
 		System.out.println();
-
+		
 		int numInput = 1 ; 
-
-		
-
+		//starts the game
 		while (numInput == 1 ) {
-
 			play() ; 
-
-
-
-			System.out.println(roundWinner()); //shows either if the dealer or user wins the hand
+			System.out.println(roundWinner());
 
 			System.out.println();
 
+			System.out.println( "Would like to continue playing? Type 0 for no or 1 for yes.");
 
+			Scanner scanner = new Scanner(System.in) ; 
 
-			System.out.println( "Would like to continue playing? Type 0 for no or 1 for yes."); 
-            //asks if the user wants to play again using a scanner, 0 for no and 1 for yes
-
-			Scanner in = new Scanner(System.in) ; 
-
-			 numInput = in.nextInt() ; //true or false using number values
-
-
-
+			numInput = scanner.nextInt() ; 
+			//asks the user if they want to continue playing
 		}
-
-		
-
-		
-
-		
-
-		if(numInput == 1 ) { //starts the game as the user wanting to play is true
-
-			play() ; 
-
-			System.out.println(roundWinner()); //shows either if the dealer or user wins the hand
-
-			System.out.println();
-
-			System.out.println( "Thanks for playing. Bye bye."); 
-
-			
-
-			
-
-		}
-
-		
-
-		
-
+		// goodbye message if player doesn't want to continue playing
 		if(numInput == 0) {
-
-			System.out.println( "See you later. Please play again soon!"); 
-            // goodbye message if player doesn't want to continue playing
-
+			System.out.println( "See you later. Please play again soon!");
+			//prints the goodbye message if the user doesn't want to continue playing
 		}
+	}
+	public static int dealerTopCard() {
+		int dealerFirstCard = (int) (Math.random() * 13) + 1 ; 
 
+		return dealerFirstCard;
+		//deals the first card of the dealer and reveals to user
+	}
+	public static int dealerSecCard() {
+		int dealerSecondCard = (int) (Math.random() * 13) + 1 ; 
 
+		return dealerSecondCard ; 
+		//deals the second card of the dealer and is hidden to the user
 
+	}
+	public static int playerTopCard() {
+		int playerFirstCard = (int) (Math.random() * 13) + 1 ; 
+
+		return playerFirstCard;
+		//deals the first card of the player and returns the card
+	}
+	public static int playerSecCard() {
+		int secCard = (int) (Math.random() * 13) + 1 ; 
+
+		return secCard ; 
+		//deals the second card of the player and returns the card
+	} 
+	public static String winner() {int player = difference( playerTopCard(), playerSecCard()) ; 
+
+		int dealer = difference( dealerTopCard(), dealerSecCard()) ; 
+		//finds the difference between the cards of the player and dealer
+		
+
+		if( player < dealer ) {
+			playerCounter++ ;
+
+			return("You win this hand!" );
+			//if the player has a lower difference than the dealer, the player wins the hand 
+		}
+		else if( player == dealer ) {
+
+			return( "Draw!" ) ;
+			//if the player and dealer have the same difference, it is a draw
+		}
+		else {
+			dealerCounter++ ; 
+			return( "You lose this hand. Better luck next time!" );
+			//if the player has a higher difference than the dealer, the player loses the hand 
+		}
 	}
 
 	
 
-	public static int dealer() {
-
-		// generate a random number from 0 to 12 for the dealer 
-
-		// this will be the set dealer's hand; dealer will not hit anymore
-
-		int random = (int) (Math.random() * 12) ; 
-
-		// return the value
-
-		return random ; 
-
-	}
-
-	
-
-	public static int player() {
-
-		// lowest possible value for a card is 1 aka ace 
-
-		int firstCard = (int) (Math.random() * 13) + 1 ; 
-        //generates the first random card value from 1 to 13 mocking the cards in a deck
-
-		int secondCard = (int) (Math.random() * 13) + 1 ; 
-        //generates the second random card value from 1 to 13 mocking the cards in a deck
-
-		
-
-		
-
-		int difference = Math.abs(secondCard - firstCard) ; 
-        //finds the difference between the two cards, uses the absolute value to make sure it is positive
-		return difference ; 
-
-	}
-
-	
-
-	public static String winner() {
-
-		
-
-		if( player() < dealer() ) {
+	public static String winner( int player, int dealer ) {
+		if( player < dealer ) {
 
 			playerCounter++ ;
 
 			return("You win this hand!" );
-            //if the player's hand is less than the dealer's hand, the player wins
-		} 
-
-		
-
-		else if( player() == dealer() ) {
-
-			return( "Draw!" ) ;
-            //if the player's hand is equal to the dealer's hand, it is a draw
+			//if the player has a lower difference than the dealer, the player wins the hand and counters goes up by 1
 		}
 
-		
+		else if( player == dealer ) {
+			return( "Draw!" ) ;
+			//if the player and dealer have the same difference, it is a draw
+		}
 
 		else {
-
 			dealerCounter++ ; 
 
 			return( "You lose this hand. Better luck next time!" );
-            //if the player's hand is greater than the dealer's hand, the dealer wins
+			//if the player has a higher difference than the dealer, the player loses the hand and counters goes up by 1
 		}
-
 	}
-
-	
-
 	public static String roundWinner() {
-
 		if (playerCounter > dealerCounter) {
 
-			return("Results: You win!") ; 
-            //if the player wins more hands than the dealer, the player wins
+			return("Overall Results: You win!") ; 
+			//if the player has more wins than the dealer, the player wins the game
 		}
-
-		
-
 		else if(playerCounter < dealerCounter) {
 
-			return("Results: You lose!") ; 
-            //if the player loses more hands than the dealer, the dealer wins.
+			return("Overall Results: You lose!") ; 
+			//if the player has less wins than the dealer, the player loses the game
 		}
-
-		
-
-		else { return ("Results: Tie!") ; }
-        //if the player and dealer have the same number of wins, it is a tie
-
-		
-
+		else { return ("Overall Results: Tie!") ; }
+		//if the player and dealer have the same number of wins, it is a tie
 	}
-
-	
-
 	public static void play() {
-
 		System.out.println();
 
-		System.out.println("Hand " + numHands); //shows the number of hands the user and dealer have played
-
+		System.out.println("Hand " + numHands);
+		//prints the number of the hands played
 		System.out.println("-------------------------");
 
-		System.out.println( "Dealer's hand is: " + dealer() ); //shows the dealer's hand
+		int d1 = dealerTopCard() ;
 
-		System.out.println( "Your hand is: " + player() ) ; //shows the player's hand
+		int d2 = dealerSecCard() ;
 
-		System.out.println(winner()); //shows the winner of the hand
+		int p1 = playerTopCard() ;
+
+		int p2 = playerSecCard() ;
+		//deals the cards to the player and dealer
+		int dealerDiff = difference( d1, d2 ) ;
+
+		int playerDiff = difference( p1, p2 ) ;
+		//finds the difference between the cards of the player   
+		//prints the difference between the cards of the dealer
+		System.out.println( "Your first card is: " + p1 );
+
+		System.out.println( "Your second card is: " + p2 );
+
+		System.out.println( "Your hand is: " + difference( p1, p2 ) ) ;
+
+		System.out.println("Dealer's top card: " + d1 );
 
 		System.out.println();
 
-		numHands++ ; //adds the number of hands played to the number of hands played
+		System.out.println("Would you like to hit? Type 0 for no or 1 for yes.");
+
+		Scanner scanner = new Scanner(System.in) ; 
+
+		int hitOrNot = scanner.nextInt() ; 
+
+		if( hitOrNot == 1 ) { 
+			int playerNew = hit( playerDiff ) ;  // new diff is returned 
+
+			// compare   
+
+			System.out.println("Player's new hand: " + playerNew);
+			
+			System.out.println( winner( playerNew, dealerDiff ) ); 
+
+			
+
+		}
+		else {
+			System.out.println(winner( playerDiff, dealerDiff));
+
+			System.out.println();
+
+		}
+		numHands++ ; 
+	}
+	public static int hit( int oldDiff ) {
+
+		int card = (int) (Math.random() * 13) + 1 ;
+
+		int newDiff = Math.abs(oldDiff - card) ;
+
+		System.out.println( "Your hit card is: " + card );
+
+		return newDiff ; 
 
 	}
+	public static int difference( int card1, int card2) {
+		int difference = Math.abs( card1 - card2 )  ; 
 
+		return difference ; 
 
-
-	
-
-
-
+	}
 }
-
